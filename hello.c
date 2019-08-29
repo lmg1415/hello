28f03dc63177f13b83e13774f24445883a60be65
lmg1415 <54559013+lmg1415@users.noreply.github.com>
2019-08-29 11:26:34 -0500

    hello.c
    Added "Major" along side name

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Logan Glynn
+ * Date: 2019/8/28
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Logan Glynn\n");
+
+  printf("Computer Engineering\n");
+
+  return 0;
+}
